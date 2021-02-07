c48cdb9b9f10eda5b94790337e2f9fde78db3c3f
Rey <firnasreyhan@gmail.com>
2021-02-07 16:11:20 +0700

    detail asmaul husna

--- app/src/main/java/com/android/tomboati/adapter/AsmaulHusnaAdapter.java
+++ app/src/main/java/com/android/tomboati/adapter/AsmaulHusnaAdapter.java
@@ -1,5 +1,6 @@
 package com.android.tomboati.adapter;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.tomboati.R;
 import com.android.tomboati.model.AsmaulHusnaModel;
+import com.android.tomboati.view.activity.DetailAsmaulHusnaActivity;
 
 import java.util.List;
 
@@ -32,6 +34,18 @@ public class AsmaulHusnaAdapter extends RecyclerView.Adapter<AsmaulHusnaAdapter.
         holder.ayat.setText(list.get(position).getArab());
         holder.bacaan.setText(list.get(position).getLatin());
         holder.arti.setText(list.get(position).getTerjemahan());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(v.getContext(), DetailAsmaulHusnaActivity.class);
+                myIntent.putExtra("position", position);
+                myIntent.putExtra("ayat", list.get(position).getArab());
+                myIntent.putExtra("bacaan", list.get(position).getLatin());
+                myIntent.putExtra("arti", list.get(position).getTerjemahan());
+                v.getContext().startActivity(myIntent);
+            }
+        });
     }
 
     @Override
