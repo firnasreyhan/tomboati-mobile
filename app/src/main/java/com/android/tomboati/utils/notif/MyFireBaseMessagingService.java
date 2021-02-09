e48fe315e74168da55fc3d036020974c566b90b2
Rey <firnasreyhan@gmail.com>
2021-02-10 03:28:15 +0700

    Update MyFireBaseMessagingService.java

--- app/src/main/java/com/android/tomboati/utils/notif/MyFireBaseMessagingService.java
+++ app/src/main/java/com/android/tomboati/utils/notif/MyFireBaseMessagingService.java
@@ -10,6 +10,7 @@ import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
@@ -32,62 +33,77 @@ public class MyFireBaseMessagingService extends FirebaseMessagingService {
         title = remoteMessage.getData().get("Title");
         message = remoteMessage.getData().get("Message");
 
-        final Intent intent = new Intent(this, SplashScreenActivity.class);
-        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        int notificationID = new Random().nextInt(3000);
-
+        Log.d("msg", "onMessageReceived: " + remoteMessage.getData().get("Message"));
+        Intent intent = new Intent(this, SplashScreenActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this , 303, intent,
-                PendingIntent.FLAG_ONE_SHOT);
-        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),
-                R.drawable.ic_stat_icon);
-
-        Uri notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        String channelId = "Default";
+        NotificationCompat.Builder builder = new  NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.ic_stat_icon)
+                .setContentTitle(remoteMessage.getNotification().getTitle())
+                .setContentText(remoteMessage.getNotification().getBody()).setAutoCancel(true).setContentIntent(pendingIntent);
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            setupChannels(notificationManager);
-            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
-                    .setSmallIcon(R.drawable.ic_stat_icon)
-                    .setLargeIcon(largeIcon)
-                    .setContentTitle(title)
-                    .setContentText(message)
-                    .setAutoCancel(true)
-                    .setSound(notificationSoundUri)
-                    .setVibrate(new long[]{1000, 300, 1000})
-                    .setContentIntent(pendingIntent);
-
-            //Set notification color to match your app color template
-//            notificationBuilder.setColor(getResources().getColor(R.color.dark_green));
-            notificationManager.notify(notificationID, notificationBuilder.build());
-        } else {
-            NotificationCompat.Builder builder =
-                    new NotificationCompat.Builder(getApplicationContext())
-                            .setLargeIcon(largeIcon)
-                            .setSmallIcon(R.drawable.ic_stat_icon)
-                            .setContentTitle(title)
-                            .setContentText(message)
-                            .setAutoCancel(true)
-                            .setSound(notificationSoundUri)
-                            .setVibrate(new long[]{1000, 300, 1000})
-                            .setContentIntent(pendingIntent);
-                    ;
-            NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-            manager.notify(0, builder.build());
+            NotificationChannel channel = new NotificationChannel(channelId, "Default channel", NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
         }
+        manager.notify(0, builder.build());
+//        final Intent intent = new Intent(this, SplashScreenActivity.class);
+//        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+//        int notificationID = new Random().nextInt(3000);
+//
+//        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//        PendingIntent pendingIntent = PendingIntent.getActivity(this , 303, intent,
+//                PendingIntent.FLAG_ONE_SHOT);
+//        Bitmap largeIcon = BitmapFactory.decodeResource(getResources(),
+//                R.drawable.ic_stat_icon);
+//
+//        Uri notificationSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            setupChannels(notificationManager);
+//            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, ADMIN_CHANNEL_ID)
+//                    .setSmallIcon(R.drawable.ic_stat_icon)
+//                    .setLargeIcon(largeIcon)
+//                    .setContentTitle(title)
+//                    .setContentText(message)
+//                    .setAutoCancel(true)
+//                    .setSound(notificationSoundUri)
+//                    .setVibrate(new long[]{1000, 300, 1000})
+//                    .setContentIntent(pendingIntent);
+//
+//            //Set notification color to match your app color template
+////            notificationBuilder.setColor(getResources().getColor(R.color.dark_green));
+//            notificationManager.notify(notificationID, notificationBuilder.build());
+//        } else {
+//            NotificationCompat.Builder builder =
+//                    new NotificationCompat.Builder(getApplicationContext())
+//                            .setLargeIcon(largeIcon)
+//                            .setSmallIcon(R.drawable.ic_stat_icon)
+//                            .setContentTitle(title)
+//                            .setContentText(message)
+//                            .setAutoCancel(true)
+//                            .setSound(notificationSoundUri)
+//                            .setVibrate(new long[]{1000, 300, 1000})
+//                            .setContentIntent(pendingIntent);
+//                    ;
+//            NotificationManager manager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+//            manager.notify(0, builder.build());
+//        }
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void setupChannels(NotificationManager notificationManager){
-        CharSequence adminChannelName = "New notification";
-        String adminChannelDescription = "Device to devie notification";
-
-        NotificationChannel adminChannel;
-        adminChannel = new NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_HIGH);
-        adminChannel.setDescription(adminChannelDescription);
-        adminChannel.enableLights(true);
-        adminChannel.setLightColor(Color.RED);
-        adminChannel.enableVibration(true);
-        if (notificationManager != null) {
-            notificationManager.createNotificationChannel(adminChannel);
-        }
-    }
+//    @RequiresApi(api = Build.VERSION_CODES.O)
+//    private void setupChannels(NotificationManager notificationManager){
+//        CharSequence adminChannelName = "New notification";
+//        String adminChannelDescription = "Device to devie notification";
+//
+//        NotificationChannel adminChannel;
+//        adminChannel = new NotificationChannel(ADMIN_CHANNEL_ID, adminChannelName, NotificationManager.IMPORTANCE_HIGH);
+//        adminChannel.setDescription(adminChannelDescription);
+//        adminChannel.enableLights(true);
+//        adminChannel.setLightColor(Color.RED);
+//        adminChannel.enableVibration(true);
+//        if (notificationManager != null) {
+//            notificationManager.createNotificationChannel(adminChannel);
+//        }
+//    }
 }
