06ff40c28f90ed08aefcd7fbcd4cf408f4a88c54
Rey <firnasreyhan@gmail.com>
2021-02-24 20:12:29 +0700

    up

--- app/src/main/java/com/android/tomboati/view/fragment/AkunFragment.java
+++ app/src/main/java/com/android/tomboati/view/fragment/AkunFragment.java
@@ -12,7 +12,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -39,7 +39,7 @@ public class AkunFragment extends Fragment {
     private AkunViewModel akunViewModel;
     private TextInputEditText textInputEditTextEmail, textInputEditTextPassword;
     private MaterialButton materialButtonSignIn, materialButtonSignOut;
-    private ConstraintLayout constraintLayoutSignIn;
+    private NestedScrollView nestedScrollViewSignIn;
     private LinearLayout linearLayoutAkun;
     private ShapeableImageView shapeableImageViewFoto;
     private TextView textViewNamaLengkap, textViewNomorHP;
@@ -56,7 +56,7 @@ public class AkunFragment extends Fragment {
         textInputEditTextEmail = view.findViewById(R.id.textInputEditTextEmail);
         textInputEditTextPassword = view.findViewById(R.id.textInputEditTextPassword);
         materialButtonSignIn = view.findViewById(R.id.materialButtonSignIn);
-        constraintLayoutSignIn = view.findViewById(R.id.constraintLayoutSignIn);
+        nestedScrollViewSignIn = view.findViewById(R.id.constraintLayoutSignIn);
         linearLayoutAkun = view.findViewById(R.id.linearLayoutAkun);
         shapeableImageViewFoto = view.findViewById(R.id.shapeableImageViewFoto);
         textViewNamaLengkap = view.findViewById(R.id.textViewNamaLengkap);
@@ -65,7 +65,7 @@ public class AkunFragment extends Fragment {
 
         if (AppPreference.getUser(getContext()) != null) {
 //            ((MainActivity)getActivity()).updateStatusBarColor("#00441F");
-            constraintLayoutSignIn.setVisibility(View.GONE);
+            nestedScrollViewSignIn.setVisibility(View.GONE);
             linearLayoutAkun.setVisibility(View.VISIBLE);
 
             setAkun();
@@ -98,7 +98,7 @@ public class AkunFragment extends Fragment {
                 }
             });
         } else {
-            constraintLayoutSignIn.setVisibility(View.VISIBLE);
+            nestedScrollViewSignIn.setVisibility(View.VISIBLE);
             linearLayoutAkun.setVisibility(View.GONE);
             textViewSignUp.setOnClickListener(new View.OnClickListener() {
                 @Override
