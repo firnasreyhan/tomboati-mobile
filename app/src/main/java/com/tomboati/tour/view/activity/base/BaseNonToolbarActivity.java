a0fc5610d8d1d80b27101555c6298d813f3124ad
rizkyabdillah <rizkyaks@gmail.com>
2021-10-29 15:33:14 +0700

    [UPDATE] Add features base activity

--- app/src/main/java/com/tomboati/tour/view/activity/base/BaseNonToolbarActivity.java
+++ app/src/main/java/com/tomboati/tour/view/activity/base/BaseNonToolbarActivity.java
@@ -9,14 +9,19 @@ import android.widget.Toast;
 import androidx.annotation.CallSuper;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 import com.tomboati.tour.R;
+import com.tomboati.tour.utils.AlertProgress;
 
 public abstract class BaseNonToolbarActivity extends AppCompatActivity {
+
+    private AlertProgress alertProgress;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,23 +44,33 @@ public abstract class BaseNonToolbarActivity extends AppCompatActivity {
         startActivity(new Intent(this, cls));
     }
 
-    protected void startsActivity(Intent intent) {
-        startActivity(intent);
-    }
-
     protected void picassoLoad(String uri, ImageView imageView) {
         Picasso.get().load(uri).priority(Picasso.Priority.HIGH).placeholder(R.drawable.ic_logo).into(imageView);
-    }
-
-    protected void setToolbar(Toolbar toolbar, String title){
-        setSupportActionBar(toolbar);
-        setTitle(title);
     }
 
     protected void setRecyclerView(RecyclerView recyclerView, RecyclerView.Adapter<?> adapter) {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+    }
+
+    protected void showProgressDialog(String message) {
+        this.alertProgress = new AlertProgress(this, message);
+        this.alertProgress.showDialog();
+    }
+
+    protected void dismissProgressDialog() {
+        if(this.alertProgress.isDialogShowing()) {
+            this.alertProgress.dismissDialog();
+        }
+    }
+
+    protected void cropImages() {
+        CropImage.activity().setGuidelines(CropImageView.Guidelines.OFF).start(this);
+    }
+
+    protected LifecycleOwner getOwner() {
+        return this;
     }
 
     protected abstract View getContentView();
